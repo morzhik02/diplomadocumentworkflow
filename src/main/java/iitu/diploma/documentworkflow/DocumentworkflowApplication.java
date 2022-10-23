f1259f9fe6cf4bd7ec970fa05cbddabc2bf26d0a
Marzhan Izteleu <marzhanizteleuova@gmail.com>
2022-10-24 02:51:43 +0600

    Initial commit

---
+++ src/main/java/iitu/diploma/documentworkflow/DocumentworkflowApplication.java
@@ -0,0 +1,13 @@
+package iitu.diploma.documentworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentworkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentworkflowApplication.class, args);
+	}
+
+}
